fcc074c753f9cd859b22f54b19173a21bf48a48f
Zenibaa <52964692+Zenibaa@users.noreply.github.com>
2025-07-10 13:06:06 +0200

    chore(alten/ecommerce) : first  version

--- back/ecommerce/src/main/java/com/alten/ecommerce/config/SecurityConfig.java
+++ back/ecommerce/src/main/java/com/alten/ecommerce/config/SecurityConfig.java
@@ -17,7 +17,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -76,11 +75,9 @@ public class SecurityConfig {
         http.cors().and().csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeHttpRequests(authz -> authz
-//                        .antMatchers("**").permitAll()
                         .antMatchers("/api/auth/**").permitAll()
                         .antMatchers("/api/products").authenticated()
                         .antMatchers("/api/cart/*").authenticated()
-//                        .anyRequest().permitAll()
                         .anyRequest().authenticated()
                 );
 
@@ -89,48 +86,6 @@ public class SecurityConfig {
 
         return http.build();
     }
-//@Bean
-//public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//    http
-//            .csrf(csrf -> csrf.disable())
-//            .cors(cors -> cors.configure(http))
-//            .exceptionHandling(exception -> exception
-//                    .authenticationEntryPoint(unauthorizedHandler)
-//            )
-//            .sessionManagement(session -> session
-//                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//            )
-//            .authorizeHttpRequests(auth -> auth
-//                    .requestMatchers(
-//                            new AntPathRequestMatcher("/api/auth/**"),
-//                            new AntPathRequestMatcher("/swagger-ui/**"),
-//                            new AntPathRequestMatcher("/v3/api-docs/**")
-//                    ).permitAll()
-//                    .requestMatchers(
-//                            new AntPathRequestMatcher("/api/products/**"),
-//                            new AntPathRequestMatcher("/api/users/**")
-//                    ).authenticated()
-//                    .anyRequest().authenticated()
-//            )
-//            .authenticationProvider(authenticationProvider())
-//            .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-//
-//    return http.build();
-//}
-
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(csrf -> csrf.disable()) // ← Désactivation explicite
-//                .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers(new AntPathRequestMatcher("/api/auth/**"),
-//                                new AntPathRequestMatcher("/api/products/**"),new AntPathRequestMatcher("/api/cart/**")).permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-//
-//        return http.build();
-//    }
 
 
 }
